cec6ae874f4342ae2bb3c1477b63ecbbd8020e0e
yakultproducer <johnathanwong2001@gmail.com>
2024-05-09 21:35:15 -0700

    Implement Summon
    - throw ball
    - ball collide with ground summon InWorld AI

--- Assets/Scripts/Launcher.cs
+++ Assets/Scripts/Launcher.cs
@@ -5,23 +5,25 @@ using UnityEngine;
 public class Launcher : MonoBehaviour
 {
     public GameObject _prefab;
+    public bool isShootMode = true;
     void Update()
     {
         #if UNITY_EDITOR
-        if(Input.GetMouseButtonDown(0))
+        if(Input.GetMouseButtonDown(0) && isShootMode)
         #else
-        if (Input.touchCount > 0)
+        if (Input.touchCount > 0  && isShootMode)
         #endif
         {
             //spawn in front of at the camera
             var pos = Camera.main.transform.position;
             var forw = Camera.main.transform.forward;
-            var thing = Instantiate(_prefab, pos+(forw*0.1f), Quaternion.identity);
+            var thing = Instantiate(_prefab, pos+(forw*0.1f), Quaternion.identity, transform);
+            isShootMode = false;
 
             //if it has physics fire it!
             if (thing.TryGetComponent(out Rigidbody rb))
             {
-                rb.AddForce(forw * 200.0f);
+                rb.AddForce(forw * 100.0f);
             }
         }
     }
---
+++ Assets/Scripts/SummonObj.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SummonObj : MonoBehaviour
+{
+    private bool hit = false;
+    private bool onGround = false;
+    public GameObject summonModel;
+    
+    private Launcher launcher;
+
+    void Start()
+    {
+        Invoke("OutOfBound", 3.0f);
+        GameObject launcherScript = transform.parent.gameObject;
+        launcher = launcherScript.GetComponent<Launcher>();
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        onGround = true;
+        if (!hit)
+        {
+            Invoke("Summon", 1.0f);
+            print(collision.transform);
+            hit = true;
+        }
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        onGround = false;
+    }
+    void Summon()
+    {
+        Destroy(gameObject);
+        launcher.isShootMode = true;
+        if (onGround)
+        {
+            Vector3 direction = Camera.main.transform.position - transform.position;
+            direction.y = 0;
+            Quaternion rotation = Quaternion.LookRotation(direction, Vector3.up);
+            Instantiate(summonModel, gameObject.transform.position, rotation);
+        }
+        else
+        {
+            Debug.Log("Not on Ground");
+        }
+        
+    }
+
+    void OutOfBound()
+    {
+        Destroy(gameObject);
+        launcher.isShootMode = true;
+        Debug.Log("Out Of Bound, Scan Before You Summon.");
+    }
+
+}
